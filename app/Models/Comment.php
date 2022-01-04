f292122dd84cbe1fe470bd418f88e23c1a70360b
geobas <ksenera@yahoo.com>
2022-01-04 17:01:21 +0200

    define eloquent model for Comment entity

---
+++ app/Models/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'article_id',
+        'content',
+        'owner',
+    ];
+
+    /**
+     * A Comment belongs to an Article.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function article()
+    {
+        return $this->belongsTo(Article::class);
+    }
+}
