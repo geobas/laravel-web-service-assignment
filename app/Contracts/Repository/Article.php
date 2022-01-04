ddf6824eb0095640c95c4a73b6f6fcde629f188a
geobas <ksenera@yahoo.com>
2022-01-04 17:16:30 +0200

    specify a contract for Article repository

---
+++ app/Contracts/Repository/Article.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Contracts\Repository;
+
+use App\Models\Article as ArticleModel;
+use App\Http\Resources\Article as ArticleResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+/**
+ * Specify a set of repository methods for Article retrieval & persistence.
+ */
+interface Article
+{
+    /**
+     * Get all Articles.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     *
+     * @throws App\Exceptions\CrudException
+     */
+    public function index(): AnonymousResourceCollection;
+
+    /**
+     * Save a new Article.
+     *
+     * @return \App\Http\Resources\Article
+     *
+     * @throws App\Exceptions\CrudException
+     */
+    public function store(): ArticleResource;
+
+    /**
+     * Get one Article.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \App\Http\Resources\Article
+     */
+    public function show(ArticleModel $article): ArticleResource;
+
+    /**
+     * Edit an Article.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \App\Http\Resources\Article
+     *
+     * @throws App\Exceptions\CrudException
+     */
+    public function update(ArticleModel $article): ArticleResource;
+
+    /**
+     * Delete an Article.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \App\Http\Resources\Article
+     *
+     * @throws App\Exceptions\CrudException
+     */
+    public function destroy(ArticleModel $article): ArticleResource;
+}
