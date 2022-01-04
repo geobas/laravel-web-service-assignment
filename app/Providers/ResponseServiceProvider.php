f7f6d9ef51ce57bb33cec06fd7708d6997466d58
geobas <ksenera@yahoo.com>
2022-01-04 17:27:29 +0200

    service provider to add custom functionality at response internal component

---
+++ app/Providers/ResponseServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Helpers\HttpStatus as Status;
+use Illuminate\Support\ServiceProvider;
+
+class ResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap service.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        response()->macro('api', function ($data, $status = Status::OK) {
+            return response()->json($data ?? [], $status, [], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        });
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
