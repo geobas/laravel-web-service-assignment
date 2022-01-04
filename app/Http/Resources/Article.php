e05114d7f59f3c4649877f31c2a422a225b996f3
geobas <ksenera@yahoo.com>
2022-01-04 17:10:10 +0200

    define API transformers for Article & Comment models

---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Article extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'category' => $this->category,
+            'uuid' => $this->uuid,
+            'creation_date' => Carbon::createFromDate($this->created_at)->format('d-m-Y H:i:s'),
+            'comments' => $this->when(
+                $this->comments->isNotEmpty(),
+                $this->comments->map(fn($comment) => $comment->only(['content', 'owner']))
+            ),
+        ];
+    }
+}
---
+++ app/Http/Resources/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Comment extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'content' => $this->content,
+            'owner' => $this->owner,
+            'article' => [
+                'title' => $this->article->title,
+                'category' => $this->article->category,
+                'uuid' => $this->article->uuid,
+            ],
+        ];
+    }
+}
