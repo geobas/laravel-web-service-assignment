a70b42673ca35cc87cbccf47e886cdc92124df9f
geobas <ksenera@yahoo.com>
2022-01-14 17:28:27 +0200

    service provider for repositories binding & resolving

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Article as ArticleModel;
-use App\Models\Comment as CommentModel;
-use App\Http\Resources\Article as ArticleResource;
-use App\Http\Resources\Comment as CommentResource;
-use App\Repositories\Article as ArticleRepository;
-use App\Repositories\Comment as CommentRepository;
-use App\Contracts\Repository\Article as ArticleRepositoryContract;
-use App\Contracts\Repository\Comment as CommentRepositoryContract;
-use App\Contracts\Service\SynchronizeArticle as SynchronizeArticleServiceContract;
-use App\Services\SynchronizeArticleExternal as SynchronizeArticleExternalService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,15 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(ArticleRepositoryContract::class, function () {
-            return new ArticleRepository(new ArticleModel, new ArticleResource(collect()));
-        });
-
-        $this->app->singleton(CommentRepositoryContract::class, function () {
-            return new CommentRepository(new CommentModel, new CommentResource(collect()));
-        });
-
-        $this->app->singleton(SynchronizeArticleServiceContract::class, SynchronizeArticleExternalService::class);
+        //
     }
 
     /**
---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Models\Article as ArticleModel;
+use App\Models\Comment as CommentModel;
+use App\Services\Article as ArticleService;
+use App\Http\Resources\Article as ArticleResource;
+use App\Http\Resources\Comment as CommentResource;
+use App\Repositories\Article as ArticleRepository;
+use App\Repositories\Comment as CommentRepository;
+use App\Contracts\Service\Article as ArticleServiceContract;
+use App\Contracts\Repository\Comment as CommentRepositoryContract;
+use App\Contracts\Service\SynchronizeArticle as SynchronizeArticleServiceContract;
+use App\Services\SynchronizeArticleExternal as SynchronizeArticleExternalService;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(ArticleServiceContract::class, function () {
+            return new ArticleService(
+                new ArticleRepository(new ArticleModel),
+                new ArticleResource(collect()),
+                new SynchronizeArticleExternalService,
+            );
+        });
+
+        $this->app->singleton(CommentRepositoryContract::class, function () {
+            return new CommentRepository(
+                new CommentModel,
+                new CommentResource(collect()),
+            );
+        });
+
+        $this->app->singleton(SynchronizeArticleServiceContract::class, SynchronizeArticleExternalService::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }	
+}
